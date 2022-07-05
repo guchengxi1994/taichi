2d903653cd60bc3f8c8a996ea5b0c461d264f188
guchengxi1994 <guchengxi1994@qq.com>
2022-07-05 17:31:59 +0800

    [taichi_file_manager] add a button

--- taichi_file_manager/frontend/lib/widgets/appbar.dart
+++ taichi_file_manager/frontend/lib/widgets/appbar.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:taichi_file_manager/app_style.dart';
@@ -22,6 +23,54 @@ class MainScreenAppbar extends StatelessWidget {
       elevation: type == ScreenType.mobile ? 4 : 0,
       // actions: buildAppbarActions(),
       actions: [
+        IconButton(
+            tooltip: "报告bug",
+            onPressed: () {
+              showCupertinoDialog(
+                  context: context,
+                  builder: (context) {
+                    return UnconstrainedBox(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        height: MediaQuery.of(context).size.height * 0.6,
+                        child: Dialog(
+                          child: Column(
+                            children: [
+                              SingleChildScrollView(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Container(
+                                    margin: const EdgeInsets.all(20),
+                                    child: SelectableText(
+                                      jsonEncode(context
+                                          .read<FileTreeController>()
+                                          .structure!
+                                          .toJson()),
+                                      maxLines: null,
+                                    ),
+                                  )),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(0);
+                                      },
+                                      child: const Text("取消")),
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(1);
+                                      },
+                                      child: const Text("确定")),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            },
+            icon: const Icon(Icons.bug_report)),
         IconButton(
             tooltip: "在此处添加一个逻辑目录",
             onPressed: () {},
